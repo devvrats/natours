675ea6db9712ff2eca31f2ba39063f09b7249e5e
devvrats <dvsharma88@gmail.com>
2024-02-19 17:10:48 +0530

    updated gitignore file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,16 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModels');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.neme,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  const token = jwt.sign({ id: newUser._id, 'secret',  });
+
   res.status(201).json({
     status: 'success',
     user: newUser,
