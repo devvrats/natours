e5814d5794dbbfc6eab132f8d634ed0a0a949b12
devvrats <dvsharma88@gmail.com>
2025-03-06 07:57:43 +0530

    implemented preventing duplicate review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -21,6 +21,8 @@ const reviewSchema = new mongoose.Schema(
   { toJSON: { virtuals: true }, toObject: { virtuals: true } },
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   // this.populate({ path: 'tour', select: 'name' }).populate({
   //   path: 'user',
