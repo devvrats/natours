e099c04ac7bd91b91084116730e3a0b43d49d1aa
devvrats <dvsharma88@gmail.com>
2025-03-06 07:02:59 +0530

    implemented average and number of ratings

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -4,8 +4,6 @@ const APIFeatures = require('./../utils/apiFeatures');
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    console.log('hello');
-
     const doc = await Model.findByIdAndDelete(req.params.id);
     if (!doc) return next(new AppError('No document found with that ID', 404));
 
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,7 +1,7 @@
 //review/ rating / creaatedAt / ref to tour / ref to user
 // const { default: mongoose } = require('mongoose');
 const mongoose = require('mongoose');
-
+const Tour = require('./tourModel');
 const reviewSchema = new mongoose.Schema(
   {
     review: { type: String, require: [true, 'Can not be empty!'] },
@@ -33,6 +33,44 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function (tourID) {
+  const stats = await this.aggregate([
+    { $match: { tour: tourID } },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  // console.log(stats);
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourID, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourID, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+reviewSchema.post('save', function () {
+  // this points to current review
+  this.constructor.calcAverageRatings(this.tour);
+});
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.clone().findOne();
+  // console.log(this.r);
+  next();
+});
+reviewSchema.post(/^findOneAnd/, async function () {
+  // this.findOne(); does NOT work here, query has already executed
+  await this.r.constructor.calcAverageRatings(this.r.tour);
+});
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
